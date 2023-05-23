c7382d7a50a332a3444cffbe1183bfb7b40f4700
Yash <104668751+Yashdon999@users.noreply.github.com>
2023-05-23 23:25:05 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -204,26 +204,26 @@ check_status() {
 banner() {
 	cat <<- EOF
 		${ORANGE}
-		${ORANGE} ______      _     _     _               
-		${ORANGE}|___  /     | |   (_)   | |              
-		${ORANGE}   / / _ __ | |__  _ ___| |__   ___ _ __ 
-		${ORANGE}  / / | '_ \| '_ \| / __| '_ \ / _ \ '__|
-		${ORANGE} / /__| |_) | | | | \__ \ | | |  __/ |   
-		${ORANGE}/_____| .__/|_| |_|_|___/_| |_|\___|_|   
-		${ORANGE}      | |                                
-		${ORANGE}      |_|                ${RED}Version : ${__version__}
+		${ORANGE}__   __     _     _     _               
+		${ORANGE}\ \ / / __ | |__ (_)___| |__   ___ _ __ 
+		${ORANGE} \ V / '_ \| '_ \| / __| '_ \ / _ \ '__|
+		${ORANGE}  | || |_) | | | | \__ \ | | |  __/ |   
+		${ORANGE}  |_|| .__/|_| |_|_|___/_| |_|\___|_|   
+		${ORANGE}     |_|                                
+		${ORANGE}                                     
+		${ORANGE}                      ${RED}Version : ${__version__}
 
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by Yphisher (tahmid.rayat)${WHITE}
 	EOF
 }
 
 ## Small Banner
 banner_small() {
 	cat <<- EOF
-		${BLUE}
-		${BLUE}  ░▀▀█░█▀█░█░█░▀█▀░█▀▀░█░█░█▀▀░█▀▄
-		${BLUE}  ░▄▀░░█▀▀░█▀█░░█░░▀▀█░█▀█░█▀▀░█▀▄
-		${BLUE}  ░▀▀▀░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀${WHITE} ${__version__}
+		${BLUE}											
+		${BLUE}  ░█░░░█░█▀█░█░█░▀█▀░█▀▀░█░█░█▀▀░█▀█
+		${BLUE}  ░▀▀█▀▀░█▀▀░█▀█░░█░░▀▀█░█▀█░█▀▀░█▀▄
+		${BLUE}  ░░░█░░░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀${WHITE} ${__version__}
 	EOF
 }
 

